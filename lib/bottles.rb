ee366562c4263be900b4f1305e5e8376d21cf5a7
nilventosa <ventosa@gmail.com>
2025-04-02 18:41:30 +0200

    use switch

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,29 +1,26 @@
 class Bottles
   def verse(num)
-    if num > 2
-      "#{num} bottles of beer on the wall, " \
-      "#{num} bottles of beer.\n" \
-      "Take one down and pass it around, " \
-      "#{num - 1} bottles of beer on the wall.\n"
-    elsif num == 2
+    case num
+    when 2
       "2 bottles of beer on the wall, 2 bottles of beer.\n" \
       "Take one down and pass it around, 1 bottle of beer on the wall.\n"
-    elsif num == 1
+    when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\n" \
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    elsif num.zero?
-      "No more bottles of beer on the wall, " \
-      "no more bottles of beer.\n" \
-      "Go to the store and buy some more, " \
-      "99 bottles of beer on the wall.\n"
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" \
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    else
+      "#{num} bottles of beer on the wall, #{num} bottles of beer.\n" \
+      "Take one down and pass it around, #{num - 1} bottles of beer on the wall.\n"
     end
   end
 
-  def verses(i, j)
-    i.downto(j).map { |num| verse num }.join("\n")
+  def verses(high, low)
+    high.downto(low).map { |num| verse num }.join("\n")
   end
 
-  def song
+  def songs
     verses(99, 0)
   end
 end
