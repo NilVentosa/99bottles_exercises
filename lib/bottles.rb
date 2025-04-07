440a429dc86aeb3693d6e821193d50c23cd4a4c0
nilventosa <ventosa@gmail.com>
2025-04-07 21:10:28 +0200

    Refactor 2 to general case

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,9 +1,6 @@
 class Bottles
-  def verse(num)
-    case num
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" \
-      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+  def verse(number)
+    case number
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\n" \
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
@@ -11,8 +8,8 @@ class Bottles
       "No more bottles of beer on the wall, no more bottles of beer.\n" \
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     else
-      "#{num} bottles of beer on the wall, #{num} bottles of beer.\n" \
-      "Take one down and pass it around, #{num - 1} bottles of beer on the wall.\n"
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" \
+      "Take one down and pass it around, #{number - 1} #{container(number - 1)} of beer on the wall.\n"
     end
   end
 
@@ -22,5 +19,13 @@ class Bottles
 
   def song
     verses(99, 0)
+  end
+
+  def container(number)
+    if number == 1
+      'bottle'
+    else
+      'bottles'
+    end
   end
 end
