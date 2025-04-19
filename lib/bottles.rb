588f7e34f1748774ae188a6c41dff4fd7f1eaa05
nilventosa <ventosa@gmail.com>
2025-04-19 17:49:53 +0200

    abstract BottleVerse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,4 +1,10 @@
 class Bottles
+  attr_reader :verse_template
+
+  def initialize(verse_template: BottleVerse)
+    @verse_template = verse_template
+  end
+
   def song
     verses(99, 0)
   end
@@ -8,16 +14,36 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = BottleNumber.for(number)
+    verse_template.lyrics(number)
+  end
+end
 
+class BottleVerse
+  def self.lyrics(number)
+    new(BottleNumber.for(number)).lyrics
+  end
+
+  attr_reader :bottle_number
+
+  def initialize(bottle_number)
+    @bottle_number = bottle_number
+  end
+
+  def lyrics
     "#{bottle_number} of beer on the wall, ".capitalize +
-      "#{bottle_number} of beer.\n" \
-      "#{bottle_number.action}, " \
-      "#{bottle_number.successor} of beer on the wall.\n"
+    "#{bottle_number} of beer.\n" +
+    "#{bottle_number.action}, " +
+    "#{bottle_number.successor} of beer on the wall.\n"
   end
 end
 
 class BottleNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
   def self.for(number)
     registry.find {|candidate| candidate.handles?(number)}.new(number)
   end
@@ -36,12 +62,6 @@ class BottleNumber
 
   def self.handles?(number)
     true
-  end
-
-  attr_reader :number
-
-  def initialize(number)
-    @number = number
   end
 
   def to_s
